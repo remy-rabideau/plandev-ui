51c64a3e13bdb9df7c1d267bab2a5c6a6d34dfaf
JosephVolosin <josephvolosin@gmail.com>
2026-06-18 12:45:19 -0400

    directive => event

--- src/routes/plans/[id]/+page.svelte
+++ src/routes/plans/[id]/+page.svelte
@@ -757,8 +757,8 @@
       $activeDirectiveName = '';
     }
   }}
-  on:createActivityDirective={directive => {
-    onCreateActivityDirective(directive.detail.directive);
+  on:createActivityDirective={event => {
+    onCreateActivityDirective(event.detail.directive);
   }}
   user={$user}
 />
